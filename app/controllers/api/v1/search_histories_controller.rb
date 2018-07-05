988b8fa1e07ea13930b06000e7d6497786661c59
github username <github email address>
2018-07-05 12:38:48 -0400

    uncommented cors gem and added create search history

--- app/controllers/api/v1/search_histories_controller.rb
+++ app/controllers/api/v1/search_histories_controller.rb
@@ -1,11 +1,16 @@
 class Api::V1::SearchHistoriesController < ApplicationController
   def index
+    @search_histories = SearchHistory.all
+    render json: @earch_histories
   end
 
   def show
   end
 
   def create
+    byebug
+    @new_search = SearchHistory.create(params['keyword'])
+    render json: @new_search
   end
 
   def delete
