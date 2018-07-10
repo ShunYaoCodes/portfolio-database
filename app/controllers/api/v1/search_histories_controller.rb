7c917d5b0a028c62ebe319b253d5fc0db455f665
github username <github email address>
2018-07-09 20:25:08 -0400

    corrected recent search: newly searched item always displays on top, duplicated search gets deleted and re-added on top

--- app/controllers/api/v1/search_histories_controller.rb
+++ app/controllers/api/v1/search_histories_controller.rb
@@ -10,13 +10,19 @@ class Api::V1::SearchHistoriesController < ApplicationController
     @search_histories.each do |history|
       @symbols << Asset.all.find{|asset| asset.id == history.asset_id}.symbol
     end
-    render json: @symbols
+    render json: @symbols.reverse
   end
 
   def create
     @user = User.find(1)
     @asset = Asset.find_or_create_by(symbol: params['keyword'].upcase)
-    @new_search = @user.search_histories.find_or_create_by(asset_id: @asset.id)
+    @find = @user.search_histories.find_by(asset_id: @asset.id)
+
+    if @find
+      @user.search_histories.delete(@find)
+    end
+
+    @new_search = @user.search_histories.create(asset_id: @asset.id)
     render json: @new_search
   end
 
