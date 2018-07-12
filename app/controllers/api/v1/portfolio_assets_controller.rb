172a5a7ad3fa83063f3ebb209a58a89b263aa3f7
github username <github email address>
2018-07-11 20:18:01 -0400

    added update action for porfolio_assets_controller

--- app/controllers/api/v1/portfolio_assets_controller.rb
+++ app/controllers/api/v1/portfolio_assets_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::PortfolioAssetsController < ApplicationController
     @portfolio_asset = User.find(params[:id]).portfolio_assets
     @symbols = []
     @portfolio_asset.each do |history|
-      @symbols << {symbol: Asset.all.find{|asset| asset.id == history.asset_id}.symbol, position_type: history.position_type} 
+      @symbols << {symbol: Asset.all.find{|asset| asset.id == history.asset_id}.symbol, position_type: history.position_type}
     end
     render json: @symbols
   end
@@ -16,11 +16,17 @@ class Api::V1::PortfolioAssetsController < ApplicationController
   def create
     @user = User.find(1)
     @asset = Asset.find_or_create_by(symbol: params['symbol'].upcase)
-    @new_search = @user.portfolio_assets.find_or_create_by(asset_id: @asset.id)
-    render json: @new_search
+    @portfolio_asset = @user.portfolio_assets.find_or_create_by(asset_id: @asset.id)
+    render json: @portfolio_asset
   end
 
   def update
+    @user = User.find(params[:id])
+    @asset = Asset.find_by(symbol: params['symbol'].upcase)
+    @portfolio_asset = @user.portfolio_assets.find_by(asset_id: @asset.id)
+    @portfolio_asset[:position_type] = params['position_type']
+    @portfolio_asset.save
+    render json: @portfolio_asset
   end
 
   def destroy
