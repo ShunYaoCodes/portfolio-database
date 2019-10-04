98b5a7e119adf275edb0c4ce272b7cef5602914d
Shun Yao <shunyaocodes@gmail.com>
2019-10-03 23:04:18 -0400

    add backend user authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  
   has_many :search_histories
   has_many :watchlists
   has_many :portfolio_assets
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_11_214315) do
+ActiveRecord::Schema.define(version: 2019_10_04_024133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2018_07_11_214315) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "watchlists", force: :cascade do |t|
