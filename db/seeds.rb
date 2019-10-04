8ebfb484a61f979c08d8f09b3932a46fc794f3f9
Shun Yao <shunyaocodes@gmail.com>
2019-10-03 23:20:30 -0400

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Asset.create([{symbol: 'FB'}, {symbol: 'TWTR'}, {symbol: 'AAPL'}, {symbol: 'GOOG'}])
-user1 = User.create(username: 'shun', name: 'Shun Yao')
-user1.search_histories.create([{asset_id: 2}, {asset_id: 3}])
-user1.watchlists.create([{asset_id: 1}, {asset_id: 4}])
-user1.portfolio_assets.create([{asset_id: 2, position_type: 'Short'}, {asset_id: 1, position_type: 'Long'}, {asset_id: 3, position_type: 'Short'}, {asset_id: 4, position_type: 'Long'}])
+Asset.destroy_all
+User.destroy_all
+SearchHistory.destroy_all
+Watchlist.destroy_all
+PortfolioAsset.destroy_all
+
+for i in 0..4 do 
+    random_name = Faker::Name.unique.name
+    Asset.create([{symbol: 'FB'}, {symbol: 'TWTR'}, {symbol: 'AAPL'}, {symbol: 'GOOG'}])
+    user = User.create(username: 'shun', name: random_name)
+    user.search_histories.create([{asset_id: 2}, {asset_id: 3}])
+    user.watchlists.create([{asset_id: 1}, {asset_id: 4}])
+    user.portfolio_assets.create([{asset_id: 2, position_type: 'Short'}, {asset_id: 1, position_type: 'Long'}, {asset_id: 3, position_type: 'Short'}, {asset_id: 4, position_type: 'Long'}])
+end
+
