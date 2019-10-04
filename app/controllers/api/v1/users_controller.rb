71ad91b622920e317aae911929dea4526e38da62
Shun Yao <shunyaocodes@gmail.com>
2019-10-04 02:08:56 -0400

    update user model and controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,17 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :requires_login, only: [:index, :show]
+  before_action :requires_user_match, only: [:index, :show]
+
   def index
+    @users = User.all
+    render json: @users
   end
 
   def show
+    if is_authenticated?
+      @user = User.find(params[:id])
+      render json: @user
+    end
   end
 
   def create
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
   
   has_many :search_histories
   has_many :watchlists
