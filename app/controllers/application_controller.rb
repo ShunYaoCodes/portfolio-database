d4baf725fab2a1bda8eb4a9bcd87bdeb4128bee2
Shun Yao <shunyaocodes@gmail.com>
2019-11-03 23:59:26 -0500

    add token expiration and error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::API
     end
 
     def generate_token
-        # We will make a token here!
         payload = {
             username: @user.username,
-            id: @user.id
+            id: @user.id,
+            exp: 2.hours.from_now.to_i
         }
 
         # IMPORTANT: set nil as password parameter
@@ -24,7 +24,10 @@ class ApplicationController < ActionController::API
             decoded_token = JWT.decode token, get_secret(), true, { algorithm: 'HS256' }
             # Check decoded_token payload for expiration date.
         rescue JWT::DecodeError
-            return nil
+            return {error: "Invalid Token"}
+        rescue JWT::ExpiredSignature
+            # Handle expired token, e.g. logout user or deny access
+            return {error: "Token Expired"}
         end
 
         decoded_token
@@ -35,10 +38,17 @@ class ApplicationController < ActionController::API
     end
 
     def requires_login
-        if !is_authenticated?
-            render json: {
-            message: "No good"
-            }, status: :unauthorized
+        if decode_error = get_decoded_token[:error]
+            case decode_error
+            when "Invalid Token"
+                render json: {
+                message: "Invalid Token"
+                }, status: :unauthorized
+            when "Token Expired"
+                render json: {
+                message: "Token Expired"
+                }, status: :unauthorized
+            end
         end
     end
 
