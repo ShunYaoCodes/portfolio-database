24f1ddc3d6437103962f9de71acefd4aac3c71f0
Shun Yao <shunyaocodes@gmail.com>
2019-11-11 22:03:26 -0500

    return user first_name and last_name in sessions controller to front end

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -6,7 +6,9 @@ class Api::V1::SessionsController < ApplicationController
     
           render json: {
             token: token,
-            id: @user.id
+            id: @user.id,
+            firstName: @user.first_name,
+            lastName: @user.last_name,
           }
         else
           render json: {
